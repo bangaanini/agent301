af0a6051d7e7e4a53c51b08862634f64b6ed5bfb
bangaanini <bangaan2509@gmail.com>
2024-10-19 17:07:03 +0000

    Add files via upload

---
+++ data/config.py
@@ -0,0 +1,48 @@
+# api id, hash
+API_ID = 27226851
+API_HASH = 'e6ebc4d5d34a8291d5c0866ff98eb03d'
+
+USE_TG_BOT = False # True if you want use tg, else False
+BOT_TOKEN = '' # API TOKEN get in @BotFather
+CHAT_ID = '5789695336' # Your telegram id
+
+# penundaan antara koneksi ke akun
+ACC_DELAY = [5, 150]
+
+# тип прокси
+PROXY_TYPE = "socks5" # http/socks5
+
+# папка с сессиями (не менять)
+WORKDIR = "sessions/"
+
+# использование прокси
+USE_PROXY = False # True/False
+# проверять прокси
+CHECK_PROXY = False
+
+#то что идет после startapp=
+REF_CODE = "onetime5789695336"
+
+# мини задержка 
+MINI_SLEEP = [7, 8] #[min, max]
+
+# задержка между тасками
+TASK_SLEEP = [25, 50] #[min, max]
+
+# макимальное количество спинов в рулетке за круг
+MAX_SPIN_PER_CYCLE = 50
+
+# задержка после круга
+BIG_SLEEP = [24*60*60,28*60*60]
+
+# таски которые он будет скипать
+BLACKLIST = {'stars_purchase', 'invite_3_friends', 'transaction', 'boost'}
+
+hello ='''              _                               __  _        
+ _ __    ___ | |_  _   _   __ _  ___   ___   / _|| |_  ___ 
+| '_ \  / _ \| __|| | | | / _` |/ __| / _ \ | |_ | __|/ __|
+| |_) ||  __/| |_ | |_| || (_| |\__ \| (_) ||  _|| |_ \__ \\
+| .__/  \___| \__| \__, | \__,_||___/ \___/ |_|   \__||___/
+|_|                |___/        
+
+'''
